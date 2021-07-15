97a799bb2b80f3247b31da022d41c396cf20f7f3
moriw0 <tontonka2@gmail.com>
2021-07-15 23:39:29 +0000

    final first User class

--- example_user.rb
+++ example_user.rb
@@ -2,25 +2,20 @@ class User
     attr_accessor :first_name, :last_name, :email
     
     def initialize(attributes = {})
-        #@full_name = attributes[:first_name][:last_name]
         @first_name = attributes[:first_name]
         @last_name = attributes[:last_name]
         @email = attributes[:email]
-        @full_name = "#{@first_name} #{@last_name}"
-        #puts "#{@first_name}#{@last_name}"
-        #puts @full_name
     end
     
-    def showuser
-        "#{@full_name = "#{@first_name} #{@last_name}"}"
+    def full_name
+        "#{@first_name} #{@last_name}"
     end
 
     def alphabetical_name
-        @alphabetical = "#{@last_name}, #{@first_name}"
-        puts @alphabetical
+        "#{@last_name}, #{@first_name}"
     end
 
     def formatted_email
-        "#{@full_name} <#{@email}>"
+        "#{full_name} <#{@email}>"
     end
 end

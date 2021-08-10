db06657d0b991cfe89c45dd65b6266c0d2bae2cf
moriw0 <tontonka2@gmail.com>
2021-08-10 01:11:40 +0000

    finish list11.30

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
+  # /accout_activation/:token/edit?email=foo@bar.comに繋がる
+  host = '7ca1be2ff633477eacf8ab395881e005.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.

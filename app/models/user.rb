57b87bf6d7775ac664a4b6b6c9928479b253469d
moriw0 <tontonka2@gmail.com>
2021-08-14 23:17:12 +0000

    finish ch12

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,9 +37,8 @@ class User < ApplicationRecord
     
     #アカウントを有効にする
     def activate
-      update_columns(activated: true, activated_at: Time.zone.now)
-      update_attribute(:activated,    true)
-      update_attribute(:activated_at, Time.zone.now)
+      update_columns(activated: true,
+                     activated_at: Time.zone.now)
     end
     
     #有効化用のメールを送信する
@@ -49,8 +48,8 @@ class User < ApplicationRecord
     
     def create_reset_digest
       self.reset_token = User.new_token
-      update_attribute(:reset_digest,  User.digest(reset_token))
-      update_attribute(:reset_sent_at, Time.zone.now)
+      update_columns(reset_digest:  User.digest(reset_token),
+                     reset_sent_at: Time.zone.now)
     end
     
     def send_password_reset_email

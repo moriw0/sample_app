c95e6cd45656d5cb800cc0a409a1d87bbbb00cca
moriw0 <tontonka2@gmail.com>
2021-08-14 22:44:24 +0000

    done list12.18

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,13 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
   
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  # @user.send_reset_email (1)userオブジェクトがメソッドを呼び出す
+  # UserMailer.password_reset(self)
+  # (2)userオブジェクトを引数に渡してメソッドを呼び出す
+  # (3)受け取ったユーザーオブジェクトをインスタンス変数に入れて
+  #メールオブジェクトを生成
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
